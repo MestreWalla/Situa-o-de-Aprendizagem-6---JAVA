365f36bd71ae3b74728d114c1605b8336233cab0
ChristopherM51 <christophermatheus1@hotmail.com>
2023-12-15 19:31:37 -0300

    .

--- guijdbcmercado/src/main/java/View/VendasPainel.java
+++ guijdbcmercado/src/main/java/View/VendasPainel.java
@@ -194,7 +194,7 @@ public class VendasPainel extends JPanel {
         // Adiciona o inputPanel ao painel principal
         add(inputPanel, BorderLayout.WEST);
 
-        // tabela de carros
+        // tabela de produtos
         JScrollPane jSPane = new JScrollPane();
         add(jSPane);
         tableModel = new DefaultTableModel(new Object[][] {},
