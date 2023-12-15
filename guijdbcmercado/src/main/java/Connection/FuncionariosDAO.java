f7c9a4e66d27a72906d2a8e39eda8502fd536274
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-12-15 18:44:44 -0300

    update

--- guijdbcmercado/src/main/java/Connection/FuncionariosDAO.java
+++ guijdbcmercado/src/main/java/Connection/FuncionariosDAO.java
@@ -16,27 +16,31 @@ public class FuncionariosDAO {
     // atributos
     private Connection connection;
     private List<ListaFuncionarios> funcionarios;
+
     // construtor
     public FuncionariosDAO() {
         this.connection = ConnectionFactory.getConnection();
     }
+
     // criar Tabela
-    public void criarTabela(){
+    public void criarTabela() {
         String sql = "CREATE TABLE IF NOT EXISTS FUNCIONARIOS_MERCADO(NOME VARCHAR(255), CPF VARCHAR (255) PRIMARY KEY, EMAIL VARCHAR(255), TELEFONE VARCHAR(13), ENDERECO VARCHAR (255))";
-        try (Statement stmt = this.connection.createStatement()){
+        try (Statement stmt = this.connection.createStatement()) {
             stmt.execute(sql);
             System.out.println("(FUNCIONARIOS) - Tabela criada com sucesso.");
         } catch (SQLException e) {
-            throw new RuntimeException("(FUNCIONARIOS) - Erro ao criar a tabela: "+e.getMessage(), e);
+            throw new RuntimeException("(FUNCIONARIOS) - Erro ao criar a tabela: " + e.getMessage(), e);
         } finally {
             ConnectionFactory.closeConnection(this.connection);
         }
     }
+
     // Listar todos os precoes cadastrados
-    public List<ListaFuncionarios> listarTodos(){
+    public List<ListaFuncionarios> listarTodos() {
         PreparedStatement stmt = null;
-        //  Declaração do objeto PreparedStatement para executar a consulta
-        ResultSet rs = null;;
+        // Declaração do objeto PreparedStatement para executar a consulta
+        ResultSet rs = null;
+        ;
         // Declaração do objeto ResultSet para armazenar os resultados da consulta
         funcionarios = new ArrayList<>();
         // Criar uma lista para armazenar os funcioanrios recuperdos do banco de dados
@@ -47,22 +51,24 @@ public class FuncionariosDAO {
             rs = stmt.executeQuery();
             // Executa a consulta e armazena os resultados no ResultSet
             while (rs.next()) {
-                // Para cada registro no ResultSet, cria um objeto funcionario com os precoes do registro
+                // Para cada registro no ResultSet, cria um objeto funcionario com os precoes do
+                // registro
                 ListaFuncionarios funcionario = new ListaFuncionarios(
-                    rs.getString("nome"),
-                    rs.getString("cpf"),
-                    rs.getString("email"),
-                    rs.getString("telefone"),
-                    rs.getString("endereco"));
+                        rs.getString("nome"),
+                        rs.getString("cpf"),
+                        rs.getString("email"),
+                        rs.getString("telefone"),
+                        rs.getString("endereco"));
                 funcionarios.add(funcionario); // Adiciona o objeto funcionario à lista de Funcionarios
             }
         } catch (SQLException ex) {
             System.out.println(ex); // Em caso de erro durante a consulta, imprime o erro
         } finally {
-            ConnectionFactory.closeConnection (connection, stmt, rs); // Fecha a conexão, PreparedStatement e o ResultSet
+            ConnectionFactory.closeConnection(connection, stmt, rs); // Fecha a conexão, PreparedStatement e o ResultSet
         }
         return funcionarios; // Retorna a lista de funcionarios recuperados do banco de dados
     }
+
     // Cadastrar Funcionario no banco de dados
     public void cadastrar(String nome, String cpf, String email, String telefone, String endereco) {
         PreparedStatement stmt = null;
@@ -70,19 +76,21 @@ public class FuncionariosDAO {
         String sql = "INSERT INTO FUNCIONARIOS_MERCADO (NOME, CPF, EMAIL, TELEFONE, ENDERECO) VALUES (?,?,?,?,?)";
         try {
             stmt = connection.prepareStatement(sql);
-            stmt.setString(1,nome);
-            stmt.setString(2,cpf);
-            stmt.setString(3,email);
-            stmt.setString(4,telefone);
-            stmt.setString(5,endereco);
+            stmt.setString(1, nome);
+            stmt.setString(2, cpf);
+            stmt.setString(3, email);
+            stmt.setString(4, telefone);
+            stmt.setString(5, endereco);
             stmt.executeUpdate();
-            JOptionPane.showMessageDialog(null,"(FUNCIONARIOS) - Dados inseridos com sucesso");
-        } catch (SQLException e ) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro ao inserir dados no banco de dados.", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "(FUNCIONARIOS) - Dados inseridos com sucesso");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro ao inserir dados no banco de dados.",
+                    JOptionPane.ERROR_MESSAGE);
         } finally {
             ConnectionFactory.closeConnection(connection, stmt);
         }
     }
+
     // Atualizar dados no banco de dados
     public void atualizar(String nome, String cpf, String email, String telefone, String endereco) {
         PreparedStatement stmt = null;
@@ -91,33 +99,64 @@ public class FuncionariosDAO {
         try {
             stmt = connection.prepareStatement(sql);
             // cpf é a chave primaria, não pode ser alterada.
-            stmt.setString(1,nome);
-            stmt.setString(2,cpf);
-            stmt.setString(3,email);
-            stmt.setString(4,telefone);
-            stmt.setString(5,endereco);
+            stmt.setString(1, nome);
+            stmt.setString(2, cpf);
+            stmt.setString(3, email);
+            stmt.setString(4, telefone);
+            stmt.setString(5, endereco);
             stmt.executeUpdate();
-            JOptionPane.showMessageDialog(null,"(FUNCIONARIOS) - Dados atualizados com sucesso");
+            JOptionPane.showMessageDialog(null, "(FUNCIONARIOS) - Dados atualizados com sucesso");
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null,"Erro ao atualizar dados no banco de dados");
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar dados no banco de dados");
         } finally {
             ConnectionFactory.closeConnection(connection, stmt);
         }
     }
+
     // Apagar dados do banco
-    public void apagar (String cpf) {
+    public void apagar(String cpf) {
         PreparedStatement stmt = null;
         // Define a instrução SQL parametrizada para apagar dados pelo cpf
         String sql = "DELETE FROM FUNCIONARIOS_MERCADO WHERE CPF = ?";
         try {
             stmt = connection.prepareStatement(sql);
-            stmt.setString(2,cpf);
+            stmt.setString(2, cpf);
             stmt.executeUpdate(); // Executa a instrução SQL
-            JOptionPane.showMessageDialog(null,"(FUNCIONARIOS) - Dado apagado com sucesso");
+            JOptionPane.showMessageDialog(null, "(FUNCIONARIOS) - Dado apagado com sucesso");
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null,"(FUNCIONARIOS) - Erro ao apagar dados no banco de dados");
+            JOptionPane.showMessageDialog(null, "(FUNCIONARIOS) - Erro ao apagar dados no banco de dados");
         } finally {
             ConnectionFactory.closeConnection(connection, stmt);
         }
     }
+
+    public ListaFuncionarios buscarPorUsuario(String usuario) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ListaFuncionarios funcionario = null;
+
+        try {
+            String sql = "SELECT * FROM FUNCIONARIOS_MERCADO WHERE NOME = ?";
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, usuario);
+
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                funcionario = new ListaFuncionarios(
+                        rs.getString("nome"),
+                        rs.getString("cpf"),
+                        rs.getString("email"),
+                        rs.getString("telefone"),
+                        rs.getString("endereco"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, stmt, rs);
+        }
+
+        return funcionario;
+    }
+
 }
--- guijdbcmercado/src/main/java/View/JanelaLogin.java
+++ guijdbcmercado/src/main/java/View/JanelaLogin.java
@@ -4,21 +4,34 @@ import java.util.Scanner;
 
 import javax.swing.JFrame;
 
-public class JanelaLogin extends JFrame{
+import Connection.FuncionariosDAO;
+import Model.ListaFuncionarios;
+
+public class JanelaLogin extends JFrame {
+    private FuncionariosDAO funcionariosDAO;
+
+    public JanelaLogin() {
+        this.funcionariosDAO = new FuncionariosDAO();
+    }
+
     public boolean autenticar() {
-        // Lógica de autenticação - substitua com sua implementação
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite o nome de usuário: ");
+            String usuario = scanner.nextLine();
 
-        System.out.print("Digite o nome de usuário: ");
-        String usuario = scanner.nextLine();
+            // Buscar usuário na tabela de funcionários
+            ListaFuncionarios funcionarioAutenticado = funcionariosDAO.buscarPorUsuario(usuario);
 
-        System.out.print("Digite a senha: ");
-        String senha = scanner.nextLine();
+            // Verificar se o funcionário foi encontrado e autenticado
+            boolean autenticado = funcionarioAutenticado != null;
 
-        // Adicione sua lógica de autenticação aqui
-        // Exemplo simples: Verifique se o usuário é "admin" e a senha é "admin123"
-        boolean autenticado = usuario.equals("admin") && senha.equals("admin123");
+            if (autenticado) {
+                System.out.println("Autenticação bem-sucedida para o usuário: " + usuario);
+            } else {
+                System.out.println("Falha na autenticação para o usuário: " + usuario);
+            }
 
-        return autenticado;
+            return autenticado;
+        }
     }
 }
